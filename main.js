0eed1ce0182b90fc28e5bd0f99ff7bebdcc5d6ab
Sunner Sun <sunner@gmail.com>
2016-11-25 11:32:59 +0800

    Add a big red + button

--- main.js
+++ main.js
@@ -35,9 +35,6 @@ export default class Text2Calendar extends Component {
           renderSeparator={this._renderSeparator}
         />
         <ActionButton buttonColor="rgba(231,76,60,1)">
-          <ActionButton.Item buttonColor='#9b59b6' title="New Task" onPress={() => console.log("notes tapped!")}>
-            <Icon name="md-create" style={styles.actionButtonIcon} />
-          </ActionButton.Item>
         </ActionButton>
       </View>
     );
